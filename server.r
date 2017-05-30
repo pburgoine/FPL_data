360aee589b1b94b75ca6e55f99d4f6165b161a20
PaulB <pdburgoine@gmail.com>
2017-05-30 19:54:56 +0100

    spurstextcol
    changed to white

--- server.r
+++ server.r
@@ -9,7 +9,7 @@ function(input, output) {
   dsin<-read.csv(urlfile)
   dsin$Color<-as.character(dsin$Color)
   
-  keepCols <- reactive({c("Player","Team","Color",paste("GW",input$gameWeek,sep=""),paste("CAP",input$gameWeek,sep=""),paste("PLAY",input$gameWeek,sep=""))})
+  keepCols <- reactive({c("Player","Team","Color","Text_Color",paste("GW",input$gameWeek,sep=""),paste("CAP",input$gameWeek,sep=""),paste("PLAY",input$gameWeek,sep=""))})
 
   
   dataset <- reactive({subset(dsin, !is.na(dsin[,5+input$gameWeek]),select=keepCols())})
@@ -17,7 +17,7 @@ function(input, output) {
   output$bubbles <- renderBubbles({
 
     
-    bubbles(dataset()[,4],key=dataset()$Player,label=dataset()$Player,color=dataset()$Color,tooltip=paste(paste(dataset()[,4],"Points"),dataset()[,6],dataset()[,5],sep='\n'))
+    bubbles(dataset()[,5],key=dataset()$Player,label=dataset()$Player,color=dataset()$Color, textColor = dataset()$Text_Color,tooltip=paste(paste(dataset()[,5],"Points"),dataset()[,7],dataset()[,6],sep='\n'))
   })
   
 }
